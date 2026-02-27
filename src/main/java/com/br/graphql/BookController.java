536887eea93f54b06ba6c2fefd6137154ba03e9c
vanessacardosodsantos <vanessacardoso.santos@gmail.com>
2026-02-26 21:01:29 -0300

    Add estrutura inicial utilizando Graphql

---
+++ src/main/java/com/br/graphql/BookController.java
@@ -0,0 +1,20 @@
+package com.br.graphql;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+
+    @QueryMapping
+    public Book bookById(@Argument String id) {
+        return Book.getById(id);
+    }
+
+    @SchemaMapping
+    public Author author(Book book) {
+        return Author.getById(book.authorId());
+    }
+}
